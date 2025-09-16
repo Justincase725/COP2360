2e89c872e4ec62b4a9ee061dbaa72b30bd263ddd
Justincase725 <justin.robinson@my.pbsc.edu>
2025-09-16 09:11:55 -0400

    Update BunnyDemo.cs

--- Module_3/Projects/BunnyDemo.cs
+++ Module_3/Projects/BunnyDemo.cs
@@ -1,1 +1,35 @@
+// BunnyDemo.cs
+using System;
 
+namespace Module3Demo
+{
+    public class Bunny
+    {
+        public string Name { get; set; }
+        public bool LikesCarrots { get; set; }
+        public bool LikesHumans { get; set; }
+
+        public Bunny() { }
+        public Bunny(string n) { Name = n; }
+
+        public void Describe()
+        {
+            Console.WriteLine($"{Name} - Carrots: {LikesCarrots}, Humans: {LikesHumans}");
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Object initializer
+            Bunny b1 = new Bunny { Name = "Timmy", LikesCarrots = true, LikesHumans = false };
+
+            // Constructor + initializer
+            Bunny b2 = new Bunny("Jimmy") { LikesCarrots = false, LikesHumans = true };
+
+            b1.Describe();
+            b2.Describe();
+        }
+    }
+}
